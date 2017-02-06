c932a3813f624cb048725eb1c9e45cd8e32b70f0
Yuri Aguiar Pinto <yuyu.te319@gmail.com>
2017-02-06 12:35:58 -0300

    Added a container to display .pdf files.

--- Cubix/SecondViewController.swift
+++ Cubix/SecondViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
 class SecondViewController: UIViewController {
 
+    var pdfLoc = NSURL(fileURLWithPath: Bundle.main.path(forResource: "2LookOLL", ofType: "pdf")!)
+    
+    @IBOutlet weak var pdfView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let request = NSURLRequest(url: pdfLoc as URL)
+        self.pdfView.loadRequest(request as URLRequest)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
